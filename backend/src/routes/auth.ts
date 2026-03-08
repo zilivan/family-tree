4d39764af39322b41d3814cb3cab0cd2f308d2ad
zilivan <stroielectromir@mail.ru>
2026-03-08 22:42:14 +0300

    исправил ошибку входа админов

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -161,23 +161,15 @@ router.post("/verify-code", async (req, res) => {
 // --- Вход администратора ---
 router.post("/admin-pass", (req, res) => {
   const adminCode = req.body;
-
-  if (
-    adminCode.code !== process.env.ADMIN_CODE ||
-    adminCode.code !== process.env.SUPER_ADMIN_CODE
-  ) {
-    return res.status(403).json({ error: "Неверный админ-код" });
-  }
-  let dataUser;
-  if (adminCode.code == process.env.ADMIN_CODE) {
-    dataUser = { id: "admin", isAdmin: true };
-  }
-  if (adminCode.code == process.env.SUPER_ADMIN_CODE) {
-    dataUser = { id: "admin", isSuperAdmin: true };
-  }
-
   const token = generateToken("admin", true);
-  res.json({ token, user: dataUser });
+  if (adminCode.code === process.env.ADMIN_CODE) {
+    res.json({ token, user: { id: "admin", isAdmin: true } });
+  }
+  if (adminCode.code === process.env.SUPER_ADMIN_CODE) {
+    res.json({ token, user: { id: "admin", isSuperAdmin: true } });
+  }
+
+  res.status(403).json({ error: "Неверный админ-код" });
 });
 
 // --- Вход ВРЕМЕННЫЙ ---
