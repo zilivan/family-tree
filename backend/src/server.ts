f2550fab3504bf18b454cd240793ac8b8468f966
zilivan <stroielectromir@mail.ru>
2026-04-11 14:35:00 +0300

    добавил в крос localhost

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -39,8 +39,11 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 // 🔹 2. CORS - ВТОРЫМ, но ДО всех парсеров и роутов!
 const ALLOWED_ORIGINS = [
   "http://127.0.0.1:5173",
+  "http://localhost:5173",
   "http://127.0.0.1:8080",
+  "http://localhost:8080",
   "http://127.0.0.1:3000",
+  "http://localhost:3000",
   "https://app.zilserver.uk",
   "https://zilserver.uk",
 ];
